d02064c2fe4a72bae275e89ead217200a67111d4
ValeraDanilov <delfiyets@gmail.com>
2023-04-20 14:47:22 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/Job4j/di/SpringDI.java
+++ src/main/java/ru/Job4j/di/SpringDI.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("ru.Job4j.di");
+        context.scan("ru.job4j.di");
         context.refresh();
+
+        Store store = context.getBean(Store.class);
+        store.add("Ira");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
 
         StartUI ui = context.getBean(StartUI.class);
         ui.input();
-        ui.print();
+        StartUI newUi = context.getBean(StartUI.class);
+        newUi.print();
     }
+
 }
--- src/main/java/ru/Job4j/di/StartUI.java
+++ src/main/java/ru/Job4j/di/StartUI.java
@@ -1,9 +1,10 @@
 package ru.Job4j.di;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class StartUI {
 
     /*
